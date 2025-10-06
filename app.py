c842c16ab2b534569723d602587d741b12793d29
marcaopamaral <marcaopamaral@gmail.com>
2025-10-06 15:05:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -313,10 +313,17 @@ if not st.session_state.messages:
     is_demo_df = st.session_state.df is not None and st.session_state.df.shape[0] < 1000
     
     if is_demo_df:
-        welcome_message = "Olá! Eu sou um agente desenvolvido por Marcos para o desafio I2A2. Para testes uso dados de demonstração criado por mim. Se quiser outro arquivo, me informe o caminho pelo comando **Análise este arquivo CSV:** `https://link-para-o-seu-arquivo.csv`."
-    else:
-        welcome_message = "Olá! Eu sou um agente desenvolvido por Marcos para o desafio I2A2. Uso dados do arquivo `creditcard.csv`. Se quiser outro arquivo, me informe o caminho pelo comando **Análise este arquivo CSV:** `https://link-para-o-seu-arquivo.csv`."
-        
+    welcome_message = """Olá! Eu sou um agente desenvolvido por Marcos para o desafio I2A2.
+**Para testes uso dados de demonstração criado por mim.**
+**Se quiser outro arquivo, me informe o caminho pelo comando:**
+**Análise este arquivo CSV:** `https://link-para-o-seu-arquivo.csv`"""
+else:
+    welcome_message = """Olá! Eu sou um agente desenvolvido por Marcos para o desafio I2A2.
+**Uso dados do arquivo `creditcard.csv`.**
+**Se quiser outro arquivo, me informe o caminho pelo comando:**
+**Análise este arquivo CSV:** `https://link-para-o-seu-arquivo.csv`"""
+    
     st.session_state.messages.append({"role": "model", "parts": [{"text": welcome_message}]})
     st.rerun() # Reinicia para mostrar a mensagem de boas-vindas
+
 
