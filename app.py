0df373b08c02e42292a7c2dbd7c9d3a6bed9cce6
marcaopamaral <marcaopamaral@gmail.com>
2025-10-06 10:41:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -313,9 +313,10 @@ if not st.session_state.messages:
     is_demo_df = st.session_state.df is not None and st.session_state.df.shape[0] < 1000
     
     if is_demo_df:
-        welcome_message = "Olá! Eu sou o FraudGuard. No momento, estou usando um conjunto de dados de demonstração criado por mim. Posso analisar os dados por ele. Se quiser outro arquivo, me informe o caminho pelo comando **Análise este arquivo CSV:** `https://link-para-o-seu-arquivo.csv`."
+        welcome_message = "Olá! Eu sou um agente desenvolvido por Marcos para o desafio I2A2. Para testes uso dados de demonstração criado por mim. Se quiser outro arquivo, me informe o caminho pelo comando **Análise este arquivo CSV:** `https://link-para-o-seu-arquivo.csv`."
     else:
-        welcome_message = "Olá! Eu sou o FraudGuard. Tenho acesso ao DataFrame de fraudes chamado `creditcard.csv`. Posso analisar os dados por ele. Se quiser outro arquivo, me informe o caminho pelo comando **Análise este arquivo CSV:** `https://link-para-o-seu-arquivo.csv`."
+        welcome_message = "Olá! Eu sou um agente desenvolvido por Marcos para o desafio I2A2. Uso dados do arquivo `creditcard.csv`. Se quiser outro arquivo, me informe o caminho pelo comando **Análise este arquivo CSV:** `https://link-para-o-seu-arquivo.csv`."
         
     st.session_state.messages.append({"role": "model", "parts": [{"text": welcome_message}]})
     st.rerun() # Reinicia para mostrar a mensagem de boas-vindas
+
